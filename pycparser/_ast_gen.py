5db6b44a623f06c8f47e376181ada201244a3e4e
Your Name <you@example.com>
2024-09-29 14:57:52 -0300

    switched to using a pyproject.toml; tidied-up _ast_gen.py

--- pycparser/_ast_gen.py
+++ pycparser/_ast_gen.py
@@ -10,29 +10,27 @@
 # Eli Bendersky [https://eli.thegreenplace.net/]
 # License: BSD
 #-----------------------------------------------------------------
-from string import Template
+from itertools import chain
 
 
 class ASTCodeGenerator(object):
+
     def __init__(self, cfg_filename='_c_ast.cfg'):
         """ Initialize the code generator from a configuration
             file.
         """
         self.cfg_filename = cfg_filename
-        self.node_cfg = [NodeCfg(name, contents)
-            for (name, contents) in self.parse_cfgfile(cfg_filename)]
+        self.node_cfg = [NodeCfg(name, contents) for (name, contents) in self.parse_cfgfile(cfg_filename)]
 
     def generate(self, file=None):
         """ Generates the code into file, an open file buffer.
         """
-        src = Template(_PROLOGUE_COMMENT).substitute(
-            cfg_filename=self.cfg_filename)
+        file.write(_PROLOGUE_COMMENT.format(cfg_filename=self.cfg_filename))
+        file.write(_PROLOGUE_CODE)
 
-        src += _PROLOGUE_CODE
         for node_cfg in self.node_cfg:
-            src += node_cfg.generate_source() + '\n\n'
-
-        file.write(src)
+            file.write(node_cfg.generate_source())
+            file.write('\n\n')
 
     def parse_cfgfile(self, filename):
         """ Parse the configuration file and yield pairs of
@@ -82,93 +80,84 @@ class NodeCfg(object):
                 self.attr.append(entry)
 
     def generate_source(self):
-        src = self._gen_init()
-        src += '\n' + self._gen_children()
-        src += '\n' + self._gen_iter()
-        src += '\n' + self._gen_attr_names()
-        return src
+        return '\n\n'.join([
+            f"class {self.name}(Node):",
+            self._gen_init(),
+            self._gen_children(),
+            self._gen_iter(),
+            self._gen_attr_names(),
+        ])
 
     def _gen_init(self):
-        src = "class %s(Node):\n" % self.name
+        src_lines = []
 
-        if self.all_entries:
-            args = ', '.join(self.all_entries)
-            slots = ', '.join("'{0}'".format(e) for e in self.all_entries)
-            slots += ", 'coord', '__weakref__'"
-            arglist = '(self, %s, coord=None)' % args
+        if len(self.all_entries) != 0:
+            slots = ", ".join(repr(slot) for slot in chain(self.all_entries, ["coord", "__weakref__"]))
+            arglist = f'(self, {", ".join(self.all_entries)}, coord=None)'
         else:
             slots = "'coord', '__weakref__'"
             arglist = '(self, coord=None)'
 
-        src += "    __slots__ = (%s)\n" % slots
-        src += "    def __init__%s:\n" % arglist
+        src_lines.append("    __slots__ = (%s)" % slots)
+        src_lines.append("    def __init__%s:" % arglist)
 
-        for name in self.all_entries + ['coord']:
-            src += "        self.%s = %s\n" % (name, name)
+        for name in chain(self.all_entries, ['coord']):
+            src_lines.append("        self.%s = %s" % (name, name))
 
-        return src
+        return "\n".join(src_lines)
 
     def _gen_children(self):
-        src = '    def children(self):\n'
+        src_lines = ['    def children(self):']
 
-        if self.all_entries:
-            src += '        nodelist = []\n'
+        if len(self.all_entries) == 0:
+            src_lines.append('        return ()')
+            return '\n'.join(src_lines)
 
-            for child in self.child:
-                src += (
-                    '        if self.%(child)s is not None:' +
-                    ' nodelist.append(("%(child)s", self.%(child)s))\n') % (
-                        dict(child=child))
+        src_lines.append('        nodes = []')
 
-            for seq_child in self.seq_child:
-                src += (
-                    '        for i, child in enumerate(self.%(child)s or []):\n'
-                    '            nodelist.append(("%(child)s[%%d]" %% i, child))\n') % (
-                        dict(child=seq_child))
+        for child in self.child:
+            src_lines.append('        '
+                                f'if self.{child} is not None: nodes.append(("{child}", self.{child}))')
 
-            src += '        return tuple(nodelist)\n'
-        else:
-            src += '        return ()\n'
+        for child in self.seq_child:
+            src_lines.append('        '
+                                f'for i, child in enumerate(self.{child} or []):'
+                                f' nodes.append(("{child}[%d]" % i, child))')
 
-        return src
+        src_lines.append('        return tuple(nodes)')
+        return '\n'.join(src_lines)
 
     def _gen_iter(self):
-        src = '    def __iter__(self):\n'
+        src_lines = ['    def __iter__(self):']
 
-        if self.all_entries:
-            for child in self.child:
-                src += (
-                    '        if self.%(child)s is not None:\n' +
-                    '            yield self.%(child)s\n') % (dict(child=child))
-
-            for seq_child in self.seq_child:
-                src += (
-                    '        for child in (self.%(child)s or []):\n'
-                    '            yield child\n') % (dict(child=seq_child))
-
-            if not (self.child or self.seq_child):
-                # Empty generator
-                src += (
-                    '        return\n' +
-                    '        yield\n')
-        else:
+        if len(self.all_entries) == 0:
             # Empty generator
-            src += (
-                '        return\n' +
-                '        yield\n')
+            src_lines.append('        return\n        yield')
+            return '\n'.join(src_lines)
 
-        return src
+        for child in self.child:
+            src_lines.append(f'        if self.{child} is not None: yield self.{child}')
+
+        for child in self.seq_child:
+            src_lines.append(f'        yield from (self.{child} or [])')
+
+        if not (self.child or self.seq_child):
+            # Empty generator
+            src_lines.append('        return\n        yield')
+
+        return '\n'.join(src_lines)
 
     def _gen_attr_names(self):
-        src = "    attr_names = (" + ''.join("%r, " % nm for nm in self.attr) + ')'
-        return src
+        if len(self.attr) == 0:
+            return "    attr_names = ()"
+        return f"    attr_names = ({', '.join(repr(name) for name in self.attr)},)"
 
 
 _PROLOGUE_COMMENT = \
 r'''#-----------------------------------------------------------------
 # ** ATTENTION **
 # This code was automatically generated from the file:
-# $cfg_filename
+# {cfg_filename}
 #
 # Do not modify it directly. Modify the configuration file and
 # run the generator again.
@@ -185,152 +174,10 @@ r'''#-----------------------------------------------------------------
 '''
 
 _PROLOGUE_CODE = r'''
-import sys
+try: 
+    from .ast_base import Node, NodeVisitor
+except ImportError:
+    from ast_base import Node, NodeVisitor
 
-def _repr(obj):
-    """
-    Get the representation of an object, with dedicated pprint-like format for lists.
-    """
-    if isinstance(obj, list):
-        return '[' + (',\n '.join((_repr(e).replace('\n', '\n ') for e in obj))) + '\n]'
-    else:
-        return repr(obj)
-
-class Node(object):
-    __slots__ = ()
-    """ Abstract base class for AST nodes.
-    """
-    def __repr__(self):
-        """ Generates a python representation of the current node
-        """
-        result = self.__class__.__name__ + '('
-
-        indent = ''
-        separator = ''
-        for name in self.__slots__[:-2]:
-            result += separator
-            result += indent
-            result += name + '=' + (_repr(getattr(self, name)).replace('\n', '\n  ' + (' ' * (len(name) + len(self.__class__.__name__)))))
-
-            separator = ','
-            indent = '\n ' + (' ' * len(self.__class__.__name__))
-
-        result += indent + ')'
-
-        return result
-
-    def children(self):
-        """ A sequence of all children that are Nodes
-        """
-        pass
-
-    def show(self, buf=sys.stdout, offset=0, attrnames=False, nodenames=False, showcoord=False, _my_node_name=None):
-        """ Pretty print the Node and all its attributes and
-            children (recursively) to a buffer.
-
-            buf:
-                Open IO buffer into which the Node is printed.
-
-            offset:
-                Initial offset (amount of leading spaces)
-
-            attrnames:
-                True if you want to see the attribute names in
-                name=value pairs. False to only see the values.
-
-            nodenames:
-                True if you want to see the actual node names
-                within their parents.
-
-            showcoord:
-                Do you want the coordinates of each Node to be
-                displayed.
-        """
-        lead = ' ' * offset
-        if nodenames and _my_node_name is not None:
-            buf.write(lead + self.__class__.__name__+ ' <' + _my_node_name + '>: ')
-        else:
-            buf.write(lead + self.__class__.__name__+ ': ')
-
-        if self.attr_names:
-            if attrnames:
-                nvlist = [(n, getattr(self,n)) for n in self.attr_names]
-                attrstr = ', '.join('%s=%s' % nv for nv in nvlist)
-            else:
-                vlist = [getattr(self, n) for n in self.attr_names]
-                attrstr = ', '.join('%s' % v for v in vlist)
-            buf.write(attrstr)
-
-        if showcoord:
-            buf.write(' (at %s)' % self.coord)
-        buf.write('\n')
-
-        for (child_name, child) in self.children():
-            child.show(
-                buf,
-                offset=offset + 2,
-                attrnames=attrnames,
-                nodenames=nodenames,
-                showcoord=showcoord,
-                _my_node_name=child_name)
-
-
-class NodeVisitor(object):
-    """ A base NodeVisitor class for visiting c_ast nodes.
-        Subclass it and define your own visit_XXX methods, where
-        XXX is the class name you want to visit with these
-        methods.
-
-        For example:
-
-        class ConstantVisitor(NodeVisitor):
-            def __init__(self):
-                self.values = []
-
-            def visit_Constant(self, node):
-                self.values.append(node.value)
-
-        Creates a list of values of all the constant nodes
-        encountered below the given node. To use it:
-
-        cv = ConstantVisitor()
-        cv.visit(node)
-
-        Notes:
-
-        *   generic_visit() will be called for AST nodes for which
-            no visit_XXX method was defined.
-        *   The children of nodes for which a visit_XXX was
-            defined will not be visited - if you need this, call
-            generic_visit() on the node.
-            You can use:
-                NodeVisitor.generic_visit(self, node)
-        *   Modeled after Python's own AST visiting facilities
-            (the ast module of Python 3.0)
-    """
-
-    _method_cache = None
-
-    def visit(self, node):
-        """ Visit a node.
-        """
-
-        if self._method_cache is None:
-            self._method_cache = {}
-
-        visitor = self._method_cache.get(node.__class__.__name__, None)
-        if visitor is None:
-            method = 'visit_' + node.__class__.__name__
-            visitor = getattr(self, method, self.generic_visit)
-            self._method_cache[node.__class__.__name__] = visitor
-
-        return visitor(node)
-
-    def generic_visit(self, node):
-        """ Called if no explicit visitor function exists for a
-            node. Implements preorder visiting of the node.
-        """
-        for c in node:
-            self.visit(c)
 
 '''
